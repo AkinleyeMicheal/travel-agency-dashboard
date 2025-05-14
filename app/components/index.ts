f5f7cd1644e0f7775a8319489af9413deb4b4e7f
Akinleye Micheal <michealakinleye76@gmail.com>
2025-05-14 10:40:06 +0100

    Implement Routing, Dashboard Layout & UI, Auth Functionality

---
+++ app/components/index.ts
@@ -0,0 +1,6 @@
+//it is through this file that all components will be exported, hence this file needs only yo be created just once
+export { default as NavItems } from "./NavItems";
+export { default as MobileSidebar } from "./MobileSidebar";
+export { default as Header } from "./Header";
+export { default as StatsCard } from "./StatsCard";
+export { default as TripsCard } from "./TripsCard";
